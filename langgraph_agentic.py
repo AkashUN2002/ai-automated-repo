fcdfad6660b59e8742554de69b71b58284623bd5
Akash.U.N <77955504+AkashUN2002@users.noreply.github.com>
2025-08-12 11:20:55 +0530

    Add langgraph_agentic.py

---
+++ langgraph_agentic.py
@@ -0,0 +1,54 @@
+import configparser
+config = configparser.ConfigParser()
+config.read('agent_config.ini')
+
+openai_api_key = config['API_KEY']['openai_api_key']
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.llms import OpenAI
+
+llm = OpenAI(temperature=0,openai_api_key=openai_api_key)
+from dotenv import load_dotenv
+load_dotenv()
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1])
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
